82472c46d0eb0fe083fe2777ffdb993bd565b442
Alejo <aleguzzardi25@gmail.com>
2025-04-07 20:24:29 -0300

    Se agrega polly

--- Terceros.Applications/Gateways/IPeopleGateway.cs
+++ Terceros.Applications/Gateways/IPeopleGateway.cs
@@ -5,5 +5,6 @@ namespace Terceros.Applications.Gateways
     public interface IPeopleGateway
     {
         Task<PeopleDTO> GetPeople();
+
     }
 }
--- Terceros.Services/People/PeopleServices.cs
+++ Terceros.Services/People/PeopleServices.cs
@@ -1,4 +1,7 @@
 ﻿using Newtonsoft.Json;
+using Polly;
+using Polly.Retry;
+using System.Net;
 using Terceros.Applications.Configurations;
 using Terceros.Applications.DTOs.People;
 using Terceros.Applications.Gateways;
@@ -10,33 +13,36 @@ public class PeopleServices : IPeopleGateway
 {
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogServicesInteractor _logServices;
+    private readonly AsyncRetryPolicy<HttpResponseMessage> _politicaReintentos;
 
     public PeopleServices(IHttpClientFactory httpClientFactory)
     {
         _httpClientFactory = httpClientFactory;
-    }
 
+        _politicaReintentos = Policy<HttpResponseMessage>.Handle<HttpRequestException>()
+            .OrResult(respuesta => respuesta.StatusCode == HttpStatusCode.InternalServerError 
+                      || respuesta.StatusCode == HttpStatusCode.RequestTimeout)
+            .WaitAndRetryAsync(retryCount:3, 
+            intento => TimeSpan.FromSeconds(1 * intento), 
+            onRetry:(exception, conteo, contexto) =>
+            {
+                _logServices.LogError("Hora del error: " + DateTime.Now);
+            });
+    }
     public async Task<PeopleDTO> GetPeople()
     {
-        try
-        {
-            using var client = _httpClientFactory.CreateClient(ConfigHelper.ApiPeople!.HttpClientName);
-            var url = ConfigHelper.ApiPeople.Endpoint.ToString();
-            var httpsResponse = await client.GetAsync(url);
 
-            if (httpsResponse.IsSuccessStatusCode)
-            {
-                var content = await httpsResponse.Content.ReadAsStringAsync();
-                var resultDto = JsonConvert.DeserializeObject<PeopleDTO>(content);
-                return resultDto!;
-            }
+        using var client = _httpClientFactory.CreateClient(ConfigHelper.ApiPeople!.HttpClientName);
+        var url = ConfigHelper.ApiPeople.Endpoint.ToString();
 
-            return null;
-        }
-        catch (Exception ex)
+        var httpsResponse = await _politicaReintentos.ExecuteAsync(async () =>
         {
-            _logServices.LogError(ex.Message);
-            throw;
-        }
+            return await client.GetAsync(url);
+        });
+
+            var content = await httpsResponse.Content.ReadAsStringAsync();
+            var resultDto = JsonConvert.DeserializeObject<PeopleDTO>(content);
+        
+        return resultDto!;
     }
 }
